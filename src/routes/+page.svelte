df14f25ac6858748a79d60426e52a5e034cfbb0b
Aditha Doratiyawa <indegamedev@gmail.com>
2025-09-20 09:09:02 +1000

    beautified home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,34 @@
 	import { enhance } from '$app/forms';
 </script>
 
-<div class="flex h-screen items-center justify-center">
-	<div class="h-[60vh] w-1/4">
-		<h1 class="mb-8 text-4xl font-bold">It's Party Time</h1>
-		<form method="POST" use:enhance class="space-y-4">
-			<input name="title" placeholder="Event Title" class="input" minlength={1} />
-			<button class="btn btn-primary">Create a New Event</button>
-		</form>
+<div class="flex min-h-screen flex-col bg-gradient-to-t from-base-100 to-base-200">
+	<div class="flex flex-grow flex-col items-center justify-center">
+		<div class="flex flex-col gap-8">
+			<h1 class="text-6xl font-extrabold text-primary">🎉 It's Party Time</h1>
+			<section class="w-full max-w-xl rounded-2xl bg-base-100 p-10 text-center shadow-lg">
+				<h2 class="mb-4 text-2xl font-bold">Plan Your Perfect Event</h2>
+				<p class="mb-8">
+					Party Time makes it easy to create a shareable event planning page where friends can add
+					their availability, keep up with updates, and vote on polls.
+				</p>
+
+				<form class="flex flex-col justify-center gap-4 sm:flex-row" method="POST">
+					<label for="event-name" class="sr-only">Event Name</label>
+					<input
+						type="text"
+						id="title"
+						name="title"
+						placeholder="Enter your event name..."
+						class="input"
+						required
+					/>
+					<button type="submit" class="btn btn-primary"> Create Event </button>
+				</form>
+			</section>
+		</div>
 	</div>
+
+	<footer class="py-6 text-center text-sm text-gray-500">
+		&copy; <span id="year"></span> Aditha Doratiyawa. All rights reserved.
+	</footer>
 </div>
