833f37a34531d85357e6bc7b95b7bd3302221fff
Aditha Doratiyawa <indegamedev@gmail.com>
2025-09-20 20:53:55 +1000

    added favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import favicon32 from '$lib/assets/favicon-32x32.png';
+	import favicon16 from '$lib/assets/favicon-16x16.png';
 
 	let { children } = $props();
 
 	let name = $state('');
-
-	function changeName(): void {
-		localStorage.setItem('name', name);
-	}
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
 </svelte:head>
 <main class="relative min-h-screen bg-base-200">
 	{@render children?.()}
